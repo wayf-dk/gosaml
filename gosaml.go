8a4e0353e3658a51b3d0ed2bcc128c66d8230b9f
Mads Freek <freek@wayf.dk>
2022-03-08 11:43:16 +0100

    Use RequestedAuthnContext from AttributeStatement, which now is also present in the request

--- gosaml.go
+++ gosaml.go
@@ -1287,9 +1287,9 @@ func NewAuthnRequest(originalRequest, spMd, idpMd *goxml.Xp, virtualIDPID string
     		}
 		}
 
-        for _, rac := range originalRequest.QueryMulti(nil, "./samlp:RequestedAuthnContext/saml:AuthnContextClassRef") {
+        for _, rac := range originalRequest.QueryMulti(nil, `./saml:AttributeStatement/saml:Attribute[@Name="RequestedAuthnContextClassRef"]`) {
             request.QueryDashP(nil, "./samlp:RequestedAuthnContext/saml:AuthnContextClassRef[0]", rac, nil)
-            request.QueryDashP(nil, "./samlp:RequestedAuthnContext/@Comparison", originalRequest.Query1(nil, "./samlp:RequestedAuthnContext/@Comparison"), nil)
+            request.QueryDashP(nil, "./samlp:RequestedAuthnContext/@Comparison", originalRequest.Query1(nil, `./saml:AttributeStatement/saml:Attribute[@Name="RequestedAuthnContextComparison"]`), nil)
         }
 
 		if wantRequesterID {
